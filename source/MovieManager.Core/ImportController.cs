484c545dd4ad659c3c38fcb9520ade55218ecaae
Rene Lochner <school@renelochner.at>
2020-04-16 19:02:04 +0200

    Implement "ImportController"

--- source/MovieManager.Core/ImportController.cs
+++ source/MovieManager.Core/ImportController.cs
@@ -1,6 +1,10 @@
 ﻿using MovieManager.Core.Entities;
 using System;
 using System.Collections.Generic;
+using Utils;
+using System.IO;
+using System.Text;
+using System.Linq;
 
 namespace MovieManager.Core
 {
@@ -13,8 +17,31 @@ namespace MovieManager.Core
         /// </summary>
         public static IEnumerable<Movie> ReadFromCsv()
         {
-            throw new NotImplementedException();
-        }
+            string filePath = MyFile.GetFullNameInApplicationTree(Filename);
+            string[] lines = File.ReadAllLines(filePath, Encoding.UTF8);
+            IList<Movie> movies = new List<Movie>();
 
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string item = lines[i];
+                string[] parts = item.Split(';');
+                string title = parts[0];
+                int year = Convert.ToInt32(parts[1]);
+                Category category = new Category
+                {
+                    CategoryName = parts[2]
+                };
+                int duration = Convert.ToInt32(parts[3]);
+                Movie movieToAdd = new Movie
+                {
+                    Title = title,
+                    Year = year,
+                    Category = category,
+                    Duration = duration
+                };
+                movies.Add(movieToAdd);
+            }
+            return movies.ToArray();
+        }
     }
 }
